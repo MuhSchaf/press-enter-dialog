db0a6f3780e2487ce2a21ce098a6ae6dc6006472
MuhSchaf <benjamin_thiele@outlook.de>
2020-01-22 16:15:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# press-enter-dialog
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/press-enter-dialog)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{name}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class HelloComponent  {
+  @HostListener('window:keydown', ['$event'])
+  keyDown(event: KeyboardEvent) {
+		if (event.keyCode === 13)
+			this.name = "entered";
+      else if(event.code === 'ShiftLeft') {
+        this.name = "shifted";
+      }
+	}
+
+  @Input() name: string;
+}
